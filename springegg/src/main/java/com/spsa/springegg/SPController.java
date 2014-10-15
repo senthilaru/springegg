b7e61078083d9b9338697c71c427da2574c0b129
Senthil Arumugam <sarumu1@searshc.com>
2014-10-14 22:54:28 -0500

    Metrics added

--- springegg/src/main/java/com/spsa/springegg/SPController.java
+++ springegg/src/main/java/com/spsa/springegg/SPController.java
@@ -1,15 +1,24 @@
 package com.spsa.springegg;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.spsa.springegg.domain.SPInput;
+import com.spsa.springegg.domain.SPMetrics;
 
 @RestController
 public class SPController {
 
+	private @Autowired HttpServletRequest request;
+	
 	@RequestMapping(value="/name", method = RequestMethod.GET)
 	public String getName(){
 		return "SPSA";
@@ -22,4 +31,16 @@ public class SPController {
 		}
 		return "Dear " + spin.getName();
 	}
+	
+	@RequestMapping(value="/weather", method = RequestMethod.POST)
+	public String getWeather(){
+		RestTemplate rest = new RestTemplate();
+		SPMetrics metrics = (SPMetrics)request.getAttribute("metrics");
+		metrics.setClassName("SPController");
+		metrics.setOperationName("getWeather");
+		ResponseEntity<String> result = rest.exchange("http://api.openweathermap.org/data/2.5/weather?q=US,Schaumburg", HttpMethod.GET, null, String.class);
+		System.out.println("result.getBody():"+result.getBody());
+		return result.toString();
+	}
+	
 }
--- springegg/src/main/java/com/spsa/springegg/interceptor/SPInterceptor.java
+++ springegg/src/main/java/com/spsa/springegg/interceptor/SPInterceptor.java
@@ -6,22 +6,35 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import com.spsa.springegg.domain.SPMetrics;
+
 public class SPInterceptor extends HandlerInterceptorAdapter {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		System.out.println("SPSA - Pre Handle");
+		SPMetrics metrics = new SPMetrics();
+		metrics.setBrowser(request.getHeader("User-Agent"));
+		metrics.setClientIp(request.getRemoteAddr());
+		metrics.setResponseTime(System.currentTimeMillis());
+		request.setAttribute("metrics", metrics);
 		return super.preHandle(request, response, handler);
 	}
 	
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-		System.out.println("SPSA - Post Handle");
+		System.out.println("SPSA - Post Handle:");
 		super.postHandle(request, response, handler, modelAndView);
 	}
 	
 	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
 		System.out.println("SPSA - After Completion:" + ex);
+		SPMetrics metrics = (SPMetrics)request.getAttribute("metrics");
+		if(ex == null){
+			metrics.setSuccess(true);
+		}
+		metrics.setResponseTime(System.currentTimeMillis() - metrics.getResponseTime());
+		System.out.println("metrics:"+metrics);
 		super.afterCompletion(request, response, handler, ex);
 	}
 	
